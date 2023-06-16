d2f7965478793f765151c00a3d04f1a85b54f301
JosiasAOR <josiasaor@gmail.com>
2023-06-16 14:52:39 -0300

    Footer on page2

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,15 @@ export default function Home({
 }) {
   const [post, setPost] = useState([]);
 
-  useEffect(() => {
-    async function fetchPost() {
-      const post = await carregarPublicacoes()
-      console.log(post,'here Post')
-      setPost(post);
-    }
-    fetchPost();
-  }, [params.publicacoesID]);
+  
 
-  const listItens = post?.map((item: any) => (
-    <Link key={item} href={`/publicacoes/${item.id}`}>
-      <Banner key={item.id} post={item}/>
-    </Link>
-  ));
+
   
 
   return <>
   <h1 style={{marginTop:55}} >Ultimas Publicações</h1>
 
   <div style={{maxWidth:400}}>
-  {listItens}
 
   </div>
   
